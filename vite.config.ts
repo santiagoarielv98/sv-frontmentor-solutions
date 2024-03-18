b46064158cf78ea619171e6a9b6fe040f923fd7f
Santiago Ariel <santiagoarielv98@gmail.com>
2024-03-18 19:07:12 -0300

    cambiso

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: "src",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "src/index.html"),
         recipe: resolve(__dirname, "src/recipe-page/index.html"),
       },
     },
