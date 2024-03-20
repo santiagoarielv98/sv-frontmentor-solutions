b2f61604af5bc149455c0b126ad6e649d84b442e
Santiago Ariel <santiagoarielv98@gmail.com>
2024-03-20 15:00:15 -0300

    cambio en la configuracion de vite para el deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-const isDev = process.env.NODE_ENV === "development";
-const base = isDev ? "/sv-frontmentor-solutions/" : "";
+const isProd = process.env.NODE_ENV === "production";
+const base = isProd ? "/sv-frontmentor-solutions/" : "";
 
 export default defineConfig({
   base,
+  root: "src",
   plugins: [react()],
   build: {
     emptyOutDir: true,
